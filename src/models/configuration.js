7f2ad7d201a04c314cd2b1cf5a4d64a91083b3cc
Feng Yifei <fengyfei@gmail.com>
2019-11-08 15:21:45 +0800

    remove unused models

--- src/models/configuration.js
+++ src/models/configuration.js
@@ -2,19 +2,19 @@ const ConfigurationModel = {
   namespace: 'configuration',
   state: {
     defs: {
-        phaseAIndex: 0, // A 相偏移
-        phaseBIndex: 1, // B 相偏移
-        phaseCIndex: 2, // C 相偏移
+			phaseAIndex: 0, // A 相偏移
+			phaseBIndex: 1, // B 相偏移
+			phaseCIndex: 2, // C 相偏移
 
-        lineMode3P4L: 0, // 三相四线
-        lineMode3P3L: 1, // 三相三线
+			lineMode3P4L: 0, // 三相四线
+			lineMode3P3L: 1, // 三相三线
 
-        phaseSeqPositive: 0, // 正相序
-        phaseSeqNegative: 1, // 逆相序
+			phaseSeqPositive: 0, // 正相序
+			phaseSeqNegative: 1, // 逆相序
     },
     voltagePhaseDelay: [
-        [[0.0, 120.0, 240.0], [0.0, 240.0, 120.0]],
-        [[330.0, undefined, 270.0], [30.0, undefined, 90.0]],
+			[[0.0, 120.0, 240.0], [0.0, 240.0, 120.0]],
+			[[330.0, undefined, 270.0], [30.0, undefined, 90.0]],
     ],
   },
   effects: {
