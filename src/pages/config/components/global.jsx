d41ab11888ea95ca497ac4b42f8b15d177273a32
oiar <snowoiar@gmail.com>
2019-11-10 10:59:14 +0800

    test

--- src/pages/config/components/global.jsx
+++ src/pages/config/components/global.jsx
@@ -1,22 +1,22 @@
-import React, { Component } from 'react'
-import { connect } from 'dva'
-import { Row, Col, Card, InputNumber } from 'antd'
-import styles from './global.less'
+import React, { Component } from 'react';
+import { connect } from 'dva';
+import { Row, Col, Card, InputNumber, Checkbox } from 'antd';
+import styles from './global.less';
 
 const spanStyle = {
   marginRight: '12px',
-}
+};
 
 @connect(({ configuration }) => ({
   conf: configuration,
 }))
 class Global extends Component {
   renderVoltage = () => {
-    const { conf } = this.props
+    const { conf } = this.props;
 
     return (
       <div>
-        <Card title='电压滞后于 A 相电压角度配置'>
+        <Card title="电压滞后于 A 相电压角度配置">
           <Row gutter={8}>
             <Col span={6}>
               <Card title="三相四线 - 正相序">
@@ -25,27 +25,39 @@ class Global extends Component {
                     <span style={spanStyle}>一元件</span>
                     <InputNumber
                       value={
-                        conf.voltagePhaseDelay[conf.defs.lineMode3P4L][conf.defs.phaseSeqPositive][conf.defs.phaseAIndex]
+                        conf.voltagePhaseDelay[conf.defs.lineMode3P4L][conf.defs.phaseSeqPositive][
+                          conf.defs.phaseAIndex
+                        ]
                       }
-                      min={0} max={360} step={0.1} 
+                      min={0}
+                      max={360}
+                      step={0.1}
                     />
                   </p>
                   <p>
                     <span style={spanStyle}>二元件</span>
                     <InputNumber
                       value={
-                        conf.voltagePhaseDelay[conf.defs.lineMode3P4L][conf.defs.phaseSeqPositive][conf.defs.phaseBIndex]
+                        conf.voltagePhaseDelay[conf.defs.lineMode3P4L][conf.defs.phaseSeqPositive][
+                          conf.defs.phaseBIndex
+                        ]
                       }
-                      min={0} max={360} step={0.1} 
+                      min={0}
+                      max={360}
+                      step={0.1}
                     />
                   </p>
                   <p>
                     <span style={spanStyle}>三元件</span>
                     <InputNumber
                       value={
-                        conf.voltagePhaseDelay[conf.defs.lineMode3P4L][conf.defs.phaseSeqPositive][conf.defs.phaseCIndex]
+                        conf.voltagePhaseDelay[conf.defs.lineMode3P4L][conf.defs.phaseSeqPositive][
+                          conf.defs.phaseCIndex
+                        ]
                       }
-                      min={0} max={360} step={0.1} 
+                      min={0}
+                      max={360}
+                      step={0.1}
                     />
                   </p>
                 </Card.Grid>
@@ -58,27 +70,39 @@ class Global extends Component {
                     <span style={spanStyle}>一元件</span>
                     <InputNumber
                       value={
-                        conf.voltagePhaseDelay[conf.defs.lineMode3P4L][conf.defs.phaseSeqNegative][conf.defs.phaseAIndex]
+                        conf.voltagePhaseDelay[conf.defs.lineMode3P4L][conf.defs.phaseSeqNegative][
+                          conf.defs.phaseAIndex
+                        ]
                       }
-                      min={0} max={360} step={0.1} 
+                      min={0}
+                      max={360}
+                      step={0.1}
                     />
                   </p>
                   <p>
                     <span style={spanStyle}>二元件</span>
                     <InputNumber
                       value={
-                        conf.voltagePhaseDelay[conf.defs.lineMode3P4L][conf.defs.phaseSeqNegative][conf.defs.phaseBIndex]
+                        conf.voltagePhaseDelay[conf.defs.lineMode3P4L][conf.defs.phaseSeqNegative][
+                          conf.defs.phaseBIndex
+                        ]
                       }
-                      min={0} max={360} step={0.1} 
+                      min={0}
+                      max={360}
+                      step={0.1}
                     />
                   </p>
                   <p>
                     <span style={spanStyle}>三元件</span>
                     <InputNumber
                       value={
-                        conf.voltagePhaseDelay[conf.defs.lineMode3P4L][conf.defs.phaseSeqNegative][conf.defs.phaseCIndex]
+                        conf.voltagePhaseDelay[conf.defs.lineMode3P4L][conf.defs.phaseSeqNegative][
+                          conf.defs.phaseCIndex
+                        ]
                       }
-                      min={0} max={360} step={0.1} 
+                      min={0}
+                      max={360}
+                      step={0.1}
                     />
                   </p>
                 </Card.Grid>
@@ -91,43 +115,59 @@ class Global extends Component {
                     <span style={spanStyle}>一元件</span>
                     <InputNumber
                       value={
-                        conf.voltagePhaseDelay[conf.defs.lineMode3P3L][conf.defs.phaseSeqPositive][conf.defs.phaseAIndex]
+                        conf.voltagePhaseDelay[conf.defs.lineMode3P3L][conf.defs.phaseSeqPositive][
+                          conf.defs.phaseAIndex
+                        ]
                       }
-                      min={0} max={360} step={0.1} 
+                      min={0}
+                      max={360}
+                      step={0.1}
                     />
                   </p>
                   <p>
                     <span style={spanStyle}>三元件</span>
                     <InputNumber
                       value={
-                        conf.voltagePhaseDelay[conf.defs.lineMode3P3L][conf.defs.phaseSeqPositive][conf.defs.phaseCIndex]
+                        conf.voltagePhaseDelay[conf.defs.lineMode3P3L][conf.defs.phaseSeqPositive][
+                          conf.defs.phaseCIndex
+                        ]
                       }
-                      min={0} max={360} step={0.1} 
+                      min={0}
+                      max={360}
+                      step={0.1}
                     />
                   </p>
                   <p>&nbsp;</p>
                 </Card.Grid>
               </Card>
             </Col>
-            <Col span={6} style={{height: '100%'}}>
+            <Col span={6} style={{ height: '100%' }}>
               <Card title="三相三线 - 逆相序">
                 <Card.Grid>
                   <p>
                     <span style={spanStyle}>一元件</span>
                     <InputNumber
                       value={
-                        conf.voltagePhaseDelay[conf.defs.lineMode3P3L][conf.defs.phaseSeqNegative][conf.defs.phaseAIndex]
+                        conf.voltagePhaseDelay[conf.defs.lineMode3P3L][conf.defs.phaseSeqNegative][
+                          conf.defs.phaseAIndex
+                        ]
                       }
-                      min={0} max={360} step={0.1} 
+                      min={0}
+                      max={360}
+                      step={0.1}
                     />
                   </p>
                   <p>
                     <span style={spanStyle}>三元件</span>
                     <InputNumber
                       value={
-                        conf.voltagePhaseDelay[conf.defs.lineMode3P3L][conf.defs.phaseSeqNegative][conf.defs.phaseCIndex]
+                        conf.voltagePhaseDelay[conf.defs.lineMode3P3L][conf.defs.phaseSeqNegative][
+                          conf.defs.phaseCIndex
+                        ]
                       }
-                      min={0} max={360} step={0.1}
+                      min={0}
+                      max={360}
+                      step={0.1}
                     />
                   </p>
                   <p>&nbsp;</p>
@@ -137,32 +177,27 @@ class Global extends Component {
           </Row>
         </Card>
       </div>
-    )
-  }
+    );
+  };
 
   renderCurrentAngle = () => {
-    const { conf } = this.props
+    const { conf } = this.props;
 
     return (
       <div>
-        <Card title='全局配置'>
-          <Card title='正确接线时 A 相电流角度配置'>
+        <Card title="全局配置">
+          <Card title="正确接线时 A 相电流角度配置">
             <Card.Grid>
               <p>
                 <span style={spanStyle}>范围</span>
                 <InputNumber
-                  value={
-                    conf.global.currentADelayAngle
-                  }
-                  min={0} max={360.0} step={1.0} 
+                  value={conf.global.currentADelayAngle}
+                  min={0}
+                  max={360.0}
+                  step={1.0}
                 />
                 <span> - </span>
-                <InputNumber
-                  value={
-                    conf.global.currentADelayAngle + 60.0
-                  }
-                  disabled 
-                />
+                <InputNumber value={conf.global.currentADelayAngle + 60.0} disabled />
               </p>
               <p unselectable>&nbsp;</p>
               <p unselectable>&nbsp;</p>
@@ -170,24 +205,20 @@ class Global extends Component {
           </Card>
         </Card>
       </div>
-    )
-  }
+    );
+  };
 
   render() {
-    const { conf } = this.props
-  
+    const { conf } = this.props;
+
     return (
       <div className={styles.global_config}>
         <Row gutter={10}>
           {/* 电压配置 */}
-          <Col span={18}>
-            { this.renderVoltage() }
-          </Col>
+          <Col span={18}>{this.renderVoltage()}</Col>
 
           {/* 电流角配置 */}
-          <Col span={6}>
-            { this.renderCurrentAngle() }
-          </Col>
+          <Col span={6}>{this.renderCurrentAngle()}</Col>
         </Row>
       </div>
     );
