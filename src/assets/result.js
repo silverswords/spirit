4bc88fd144e947c07eb8aebea5813712a13c9677
flutterWang <wangbofeng980404@gmail.com>
2019-11-10 12:16:55 +0800

    add Table && modify Map

---
+++ src/assets/result.js
@@ -0,0 +1,40 @@
+fs = require('fs')
+path = require('path')
+
+let data = {
+  nodes: []
+}
+
+let allResult = [
+  '正确',
+  '错误一',
+  '错误二',
+  '错误三',
+  '错误四',
+  '错误五',
+  '错误六',
+]
+
+for (let i = 0; i < 100; i ++) {
+  let lng = 114.821778 + Math.random()
+  let lat = 38.473088 + Math.random()
+  let result = allResult[parseInt(Math.random()*(6))];
+  let obj = {
+    index: i,
+    name: `变电站 ${i} 号`,
+    location: [+lng.toFixed(6), +lat.toFixed(6)], 
+    result
+  }
+
+  data.nodes.push(obj)
+}
+
+let content = JSON.stringify(data);
+let file = path.join('/Users/bofeng/Documents/GitHub/sliverswords/spirit/src/assets', 'test.json')
+
+fs.writeFile(file, content, function(err) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('文件创建成功，地址：' + file)
+})
---
+++ src/pages/result/components/Table.jsx
@@ -0,0 +1,136 @@
+import React, { Component } from 'react'
+import { connect } from 'dva'
+import { Card, Table, Input, Button, Icon } from 'antd'
+import Highlighter from 'react-highlight-words';
+
+@connect(({ result }) => ({
+  conf: result,
+}))
+class ResultTable extends Component {
+  state = {
+    tmpNodes: this.props.conf.model,
+    searchText: ''
+  }
+  componentDidMount() {
+  }
+
+  getColumnSearchProps = dataIndex => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={node => {
+            this.searchInput = node;
+          }}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => this.handleSearch(selectedKeys, confirm)}
+          style={{ width: 188, marginBottom: 8, display: 'block' }}
+        />
+        <Button
+          type="primary"
+          onClick={() => this.handleSearch(selectedKeys, confirm)}
+          icon="search"
+          size="small"
+          style={{ width: 90, marginRight: 8 }}
+        >
+          搜索
+        </Button>
+        <Button onClick={() => this.handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+          重置
+        </Button>
+      </div>
+    ),
+    filterIcon: filtered => (
+      <Icon type="search" style={{ color: filtered ? '#1890ff' : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes(value.toLowerCase()),
+    onFilterDropdownVisibleChange: visible => {
+      if (visible) {
+        setTimeout(() => this.searchInput.select());
+      }
+    },
+    render: text => (
+      <Highlighter
+        highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+        searchWords={[this.state.searchText]}
+        autoEscape
+        textToHighlight={text.toString()}
+      />
+    ),
+  })
+
+  handleSearch = (selectedKeys, confirm) => {
+    confirm();
+    this.setState({ searchText: selectedKeys[0] });
+  };
+
+  handleReset = clearFilters => {
+    clearFilters();
+    this.setState({ searchText: '' });
+  };
+
+  handleChange = (extra) => {
+    const { dispatch } = this.props
+    let flitersRule = {
+      name: extra.name || [],
+      result: extra.result || []
+    }
+
+    dispatch({
+      type: 'result/filterChanged',
+      payload: flitersRule,
+    })
+  }
+
+  render() {
+    const colunms =  [
+      {
+        title: '序号',
+        dataIndex: 'index',
+        key: 'index'
+      },
+      {
+        title: '名称',
+        dataIndex: 'name',
+        key: 'name',
+        ...this.getColumnSearchProps('name')
+      },
+      {
+        title: '经纬坐标',
+        dataIndex: 'location',
+        key: 'location'
+      },
+      {
+        title: '结果',
+        dataIndex: 'result',
+        key: 'result',
+        filters: [
+          {text: '正确', value: '正确'},
+          {text: '错误一', value: '错误一'},
+          {text: '错误二', value: '错误二'},
+          {text: '错误三', value: '错误三'},
+          {text: '错误四', value: '错误四'},
+          {text: '错误五', value: '错误五'}
+        ],
+        onFilter: (value, record) => record.result === value,
+      }
+    ]
+
+    return (
+      <Card title='结果详细数据'>
+        <Table
+          dataSource={this.state.tmpNodes}
+          columns={colunms}
+          onChange={this.handleChange}
+        />
+      </Card>
+    );
+  }
+}
+
+export default ResultTable;
